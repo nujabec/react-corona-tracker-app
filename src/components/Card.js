0a65afac257fb6a110314b65a96f409522d08883
nujabec <nujabec@gmail.com>
2023-12-23 08:31:12 +0900

    WorldPageのbaseが完成

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+const Card = (props) => {
+    return (
+        <div>
+            {props.allCountriesData.map((singleData, index) =>
+                <div key={index}>
+                    <h2>{singleData.Country}</h2>
+                    <p>新規感染者: {singleData.NewConfirmed.toLocaleString()}</p>
+                    <p>感染者総数: {singleData.TotalConfirmed.toLocaleString()}</p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Card;
+
