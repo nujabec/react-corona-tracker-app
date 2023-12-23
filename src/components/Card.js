37be280cd899dca508f2732c724e1e46492a9a51
nujabec <nujabec@gmail.com>
2023-12-23 15:20:34 +0900

    done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
 const Card = (props) => {
     return (
-        <div>
+        <div className="card-container">
             {props.allCountriesData.map((singleData, index) =>
-                <div key={index}>
-                    <h2>{singleData.Country}</h2>
-                    <p>新規感染者: {singleData.NewConfirmed.toLocaleString()}</p>
-                    <p>感染者総数: {singleData.TotalConfirmed.toLocaleString()}</p>
+                <div key={index} className="card">
+                    <div>
+                        <h2>{singleData.Country}</h2>
+                        <p>新規感染者: <span>{singleData.NewConfirmed.toLocaleString()}</span></p>
+                        <p>感染者総数: <span>{singleData.TotalConfirmed.toLocaleString()}</span></p>
+                    </div>
                 </div>
             )}
         </div>
--- src/pages/WorldPage.js
+++ src/pages/WorldPage.js
@@ -1,9 +1,11 @@
+import Header from "../components/Header";
 import Title from "../components/Title";
 import Card from "../components/Card";
 
 const WorldPage = (props) => {
     return (
-        <div>
+        <div className="world-page-container">
+            <Header />
             <Title />
             <Card allCountriesData={props.allCountriesData}/>
         </div>
